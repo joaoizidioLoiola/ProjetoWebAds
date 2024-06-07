ff266ea125f55c138372c28ebd7d6cd01a4e72dc
joaoizidioLoiola <joaoizidio287@gmail.com>
2024-06-07 15:37:45 -0300

    Teste

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 const public_key = '506ed6f89fef4fafdfc895eec59c7f35'
 const private_key = 'f4cb12401e8ec6a2d8270f11cbe3a83dcac6591c';
 
-const marv = [];
 async function fetchMarvel() {
   try {
     const response = await fetch(`https://gateway.marvel.com/v1/public/comics?apikey=${public_key}`)
